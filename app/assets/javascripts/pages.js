ed52a0fa4b05275270ff4b91fd635b2689ff167a
dlmb <dan_boey@hotmail.com>
2017-10-07 00:11:34 +0800

    Init commit

---
+++ app/assets/javascripts/pages.js
@@ -0,0 +1,6 @@
+// $(document).ready(function(){
+//    var height = $(window).height();  //getting windows height
+//    console.log("height: ");
+//    console.log(height);
+//    jQuery('#myCarousel').css('height',height+'px');   //and setting height of carousel
+// });
