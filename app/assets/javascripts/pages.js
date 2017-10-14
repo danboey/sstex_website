2f0325260b4a00ff75badaf089211cca630e03d3
dlmb <dan_boey@hotmail.com>
2017-10-14 17:35:31 +0800

    All product pages

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -5,9 +5,7 @@ $(document).ready(function(){
 	});
 
 	if($('.children').length) { 
-    console.log("in");
     function switch_pics() {
-      console.log('im in the function');
       if($('.children').css('display') == 'block') {
         $('.children').hide();
         $('.ladies').fadeIn(1500);
@@ -17,6 +15,45 @@ $(document).ready(function(){
       } else {
         $('.mens').hide();
         $('.children').fadeIn(1500);
+      }
+    }
+    setInterval(switch_pics, 4000); 
+  }
+
+	if($('.woven-1').length) { 
+    function switch_pics() {
+      if($('.woven-1').css('display') == 'block') {
+        $('.woven-1').hide();
+        $('.woven-2').fadeIn(1500);
+      } else if($('.woven-2').css('display') == 'block') {
+        $('.woven-2').hide();
+        $('.woven-1').fadeIn(1500);
+      }
+    }
+    setInterval(switch_pics, 4000); 
+  }
+
+	if($('.fabrics-textiles-1').length) { 
+    function switch_pics() {
+      if($('.fabrics-textiles-1').css('display') == 'block') {
+        $('.fabrics-textiles-1').hide();
+        $('.fabrics-textiles-2').fadeIn(1500);
+      } else if($('.fabrics-textiles-2').css('display') == 'block') {
+        $('.fabrics-textiles-2').hide();
+        $('.fabrics-textiles-1').fadeIn(1500);
+      }
+    }
+    setInterval(switch_pics, 4000); 
+  }
+
+	if($('.uniforms-1').length) { 
+    function switch_pics() {
+      if($('.uniforms-1').css('display') == 'block') {
+        $('.uniforms-1').hide();
+        $('.uniforms-2').fadeIn(1500);
+      } else if($('.uniforms-2').css('display') == 'block') {
+        $('.uniforms-2').hide();
+        $('.uniforms-1').fadeIn(1500);
       }
     }
     setInterval(switch_pics, 4000); 
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get 'products/knitted' => 'pages#knitted'
+  get 'products/woven' => 'pages#woven'
+  get 'products/fabrics_textiles' => 'pages#fabrics_textiles'
+  get 'products/uniforms' => 'pages#uniforms'
+  get 'products/traditional' => 'pages#traditional'
+  
 end
