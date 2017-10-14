223a70ee32e74f1d9385912af0567f95f0e6ada5
dlmb <dan_boey@hotmail.com>
2017-10-14 14:58:46 +0800

    Init Knitted page

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
 	$('.carousel').carousel({
 	    pause: "false"
 	});
+
+	if($('.children').length) { 
+    console.log("in");
+    function switch_pics() {
+      console.log('im in the function');
+      if($('.children').css('display') == 'block') {
+        $('.children').hide();
+        $('.ladies').fadeIn(1500);
+      } else if($('.ladies').css('display') == 'block') {
+        $('.ladies').hide();
+        $('.mens').fadeIn(1500);
+      } else {
+        $('.mens').hide();
+        $('.children').fadeIn(1500);
+      }
+    }
+    setInterval(switch_pics, 4000); 
+  }
+
 });
