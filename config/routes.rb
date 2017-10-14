0711e1d98645d4f56df80d5154804b66245ec8c9
dlmb <dan_boey@hotmail.com>
2017-10-14 21:09:31 +0800

    Most other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+
   root 'pages#home'
 
-  get 'products/knitted' => 'pages#knitted'
-  get 'products/woven' => 'pages#woven'
+  get 'products/knitted' 					=> 'pages#knitted'
+  get 'products/woven' 						=> 'pages#woven'
   get 'products/fabrics_textiles' => 'pages#fabrics_textiles'
-  get 'products/uniforms' => 'pages#uniforms'
-  get 'products/traditional' => 'pages#traditional'
+  get 'products/uniforms' 				=> 'pages#uniforms'
+  get 'products/traditional' 			=> 'pages#traditional'
+
+  get 'about_us' => 'pages#about'
+  get 'clients'  => 'pages#clients'
+  get 'careers'  => 'pages#careers'
+  get 'contact'  => 'pages#contact'
   
 end
